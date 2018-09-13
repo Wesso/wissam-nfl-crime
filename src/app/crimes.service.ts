8d80662eb012be8611aae4e278d249a481cce3b0
CPHLPWMF\Wissam Maarouf <wm@kogp.dk>
2018-09-14 01:38:01 +0200

    Create the getCrimes method in the service

--- src/app/crimes.service.ts
+++ src/app/crimes.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import {Observable} from 'rxjs/Observable';
+import { Observable, of } from 'rxjs';
+
+/*
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+*/
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CrimesService {
 
-  constructor() { }
+  private crimesUrl = 'http://nflarrest.com/api/v1/crime';  // URL to NFLArrest API for crimes
+
+  constructor(private http:HttpClient) { }
+
+  getCrimes() {
+    //return this.http.get('http://nflarrest.com/api/v1/crime');
+    return this.http.get(this.crimesUrl);
+  }
 }
