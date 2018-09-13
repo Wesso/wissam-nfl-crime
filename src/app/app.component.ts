bb9f0c579b58d9e2353063fed2e3f7c2768a59de
CPHLPWMF\Wissam Maarouf <wm@kogp.dk>
2018-09-14 01:32:45 +0200

    - Change app name to nfl-app - Initialize the getCrimes function - create getCrimes function to retrieve crimes data from the crimes service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CrimesService} from './crimes.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
+  selector: 'nfl-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'NFL Crime';
+
+  public crimes;
+ 
+  constructor(private _crimesService: CrimesService) { }
+
+
+  ngOnInit() {
+    this.getCrimes();
+  }
+
+  getCrimes(): void {
+    this._crimesService.getCrimes()
+    .subscribe(crimes => this.crimes = crimes.slice(0, 10));
+  }
 }
+
+
+ 
