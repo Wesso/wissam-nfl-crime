6b10f53a9519de301a960292cc9a73e56dfe107e
CPHLPWMF\Wissam Maarouf <wm@kogp.dk>
2018-09-14 02:25:55 +0200

    Create the getTeams method in the service

--- src/app/teams.service.ts
+++ src/app/teams.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TeamsService {
 
-  constructor() { }
+  private teamsUrl = 'http://nflarrest.com/api/v1/crime/topTeams/Theft';  // URL to NFLArrest API for teams
+
+  constructor(private http:HttpClient) { }
+
+  getTeams() {
+    return this.http.get(this.teamsUrl);
+  }
 }
