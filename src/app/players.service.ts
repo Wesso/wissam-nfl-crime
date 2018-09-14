e085d0187c9b22d60e9f5c03a48a4bee11aa4808
CPHLPWMF\Wissam Maarouf <wm@kogp.dk>
2018-09-14 02:05:07 +0200

    Create the getPlayers method in the service

--- src/app/players.service.ts
+++ src/app/players.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PlayersService {
 
-  constructor() { }
+  private playersUrl = 'http://nflarrest.com/api/v1/crime/topPlayers/Theft';  // URL to NFLArrest API for players
+
+  constructor(private http:HttpClient) { }
+
+  getPlayers() {
+    return this.http.get(this.playersUrl);
+  }
 }
