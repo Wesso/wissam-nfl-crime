76fff00a782b9c696d268ebe5cfd6c76d255c277
CPHLPWMF\Wissam Maarouf <wm@kogp.dk>
2018-09-14 02:05:52 +0200

    Remove unnecessary code

--- src/app/crimes.service.ts
+++ src/app/crimes.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-//import {Observable} from 'rxjs/Observable';
 import { Observable, of } from 'rxjs';
 
-/*
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
-*/
 
 @Injectable({
   providedIn: 'root'
